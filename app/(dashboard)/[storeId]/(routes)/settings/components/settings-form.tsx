d1e4b560a7be7d3569eca656b9f14675a9cf02ef
rickychan1987 <khaivanla1987@gmail.com>
2024-03-27 07:21:46 +0700

    3h10

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,14 +1,26 @@
-"use client"
+"use client";
 
-import * as z from 'zod'
-import { Button } from '@/components/ui/button';
-import Heading from '@/components/ui/heading';
-import { Separator } from '@/components/ui/separator';
-import {Store} from '@prisma/client'
-import { Trash } from 'lucide-react';
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useState } from 'react';
+import * as z from "zod";
+import { Button } from "@/components/ui/button";
+import Heading from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { Store } from "@prisma/client";
+import { Trash } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import toast from "react-hot-toast";
+import axios from "axios";
+import { useParams, useRouter } from "next/navigation";
 
 interface SettingsFormProps {
   initialData: Store;
@@ -16,46 +28,74 @@ interface SettingsFormProps {
 
 const formSchema = z.object({
   name: z.string().min(1),
-})
+});
 
 type SettingsFormValues = z.infer<typeof formSchema>;
 
-const SettingsForm: React.FC<SettingsFormProps> = ({
-  initialData
-}) => {
-  const [open, setOpen] = useState(false)
-  const [loading, setLoading] = useState(false)
-
+const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
+  const params = useParams();
+  const router = useRouter();
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const form = useForm<SettingsFormValues>({
     resolver: zodResolver(formSchema),
-    defaultValues: initialData
-  })
+    defaultValues: initialData,
+  });
 
   const onSubmit = async (data: SettingsFormValues) => {
-    console.log(data);
-  }
+    try {
+      setLoading(true);
+      await axios.patch(`/api/stores/${params.storeId}`, data);
+      router.refresh();
+      toast.success("Store updated.");
+    } catch (error) {
+      toast.error("Something went wrong.");
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <>
-    <div className='flex items-center justify-between'>
-      <Heading 
-        title='Settings'
-        description='Manage store preferences'
-      />
-      <Button
-        variant="destructive"
-        size="icon"
-        onClick={() => {}}
-      >
-        <Trash className="h-4 w4"/>
-      </Button>
-    </div>
-    <Separator />
+      <div className="flex items-center justify-between">
+        <Heading title="Settings" description="Manage store preferences" />
+        <Button variant="destructive" size="icon" onClick={() => setOpen(true)}>
+          <Trash className="h-4 w4" />
+        </Button>
+      </div>
+      <Separator />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 w-full"
+        >
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder="Store name"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button disabled={loading} className="ml-auto" type="submit">
+            Save change
+          </Button>
+        </form>
+      </Form>
     </>
-  )
-}
+  );
+};
 
-export default SettingsForm
-//2h50 add <Form> tag
-
+export default SettingsForm;
